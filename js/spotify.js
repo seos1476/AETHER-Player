ff3ee369d341b61c79078788f09a5bc9a1689cb8
seos1476 <seos1476@gmail.com>
2026-07-11 20:47:00 +0530

    Create spotify.js

---
+++ js/spotify.js
@@ -0,0 +1,12 @@
+const SPOTIFY_CLIENT_ID = "4b760b62a55545bba8b5f80a7e4a3ff0";
+
+const REDIRECT_URI = "https://seos1476.github.io/AETHER-Player/";
+
+const SCOPES = [
+  "user-read-private",
+  "user-read-email",
+  "user-read-playback-state",
+  "user-modify-playback-state",
+  "playlist-read-private",
+  "user-library-read"
+];
