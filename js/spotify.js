f25619619c0f1c22890e1c190ad46418e0147c19
seos1476 <seos1476@gmail.com>
2026-07-11 20:48:46 +0530

    Add CLIENT_ID and REDIRECT_URI constants

--- js/spotify.js
+++ js/spotify.js
@@ -10,3 +10,5 @@ const SCOPES = [
   "playlist-read-private",
   "user-library-read"
 ];
+const CLIENT_ID = "4b760b62a55545bba8b5f80a7e4a3ff0";
+const REDIRECT_URI = "https://seos1476.github.io/AETHER-Player/";
